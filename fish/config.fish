cf87d8799b7b9ceb7568a95482d57a85cd88913f
dqn <dqn270@gmail.com>
2022-02-22 10:02:34 +0900

    chore: update

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,3 @@
-set PATH /usr/local/bin /usr/sbin ~/go/bin $PATH
+set PATH /usr/local/bin /usr/sbin ~/go/bin ~/.cargo/bin $PATH
 set GOPATH /Users/dqn/go
+status --is-interactive; and source (anyenv init -|psub)
