04b23c79ddc79e93b2a1666191d346b4c608d86e
dqn <dqn270@gmail.com>
2022-10-22 08:20:43 +0900

    chore: add rover path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,3 @@
-set PATH /usr/local/bin /usr/sbin ~/go/bin ~/.cargo/bin $PATH
+set PATH /usr/local/bin /usr/sbin ~/go/bin ~/.cargo/bin /Users/dqn/.rover/bin $PATH
 set GOPATH /Users/dqn/go
 status --is-interactive; and source (anyenv init -|psub)
